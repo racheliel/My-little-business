f443e642d8aad87d718a4159c7b1a3855a7951b9
racheliel <racheli7722@gmail.com>
2015-05-25 19:16:17 +0300

    try todo login

--- MyLittleBuisness/web/pages/homeUserIn.py
+++ MyLittleBuisness/web/pages/homeUserIn.py
@@ -1,4 +1,4 @@
-#from google.appengine.api import users
+from google.appengine.api import users
 from google.appengine.ext.webapp import template
 
 from models.user import User
@@ -7,13 +7,13 @@ import webapp2
 class IndexHandler(webapp2.RequestHandler):
 	def get(self):
 		template_params = {}
-	#	user = User.checkToken()
-	#	if not user:
-	#		template_params['loginUrl'] = User.loginUrl()
-	#		html = template.render("web/templates/sign.html", template_params)
-	#	else:
-	#		template_params['logoutUrl'] = User.logoutUrl()
-	#		template_params['user'] = user.email
+#		user = "racheli"
+#		if not user:
+#			template_params['login'] = User.login()
+#			html = template.render("web/templates/sign.html", template_params)
+#		else:
+#			template_params['logout'] = User.logout()
+#			template_params['user'] = user.email
 			html = template.render("web/templates/homeUserIn.html", template_params)
 	
 		self.response.write(html)
