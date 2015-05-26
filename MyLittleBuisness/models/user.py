cc2d6d00ba0201126e36b367f8215a651be04285
michalgi <michalgi1991@gmail.com>
2015-05-26 10:50:12 +0300

    Revert "comit to revert"
    This reverts commit 74858200dd05d8b3d1d4dfc5885d51e80c23579c.

--- MyLittleBuisness/models/user.py
+++ MyLittleBuisness/models/user.py
@@ -23,7 +23,3 @@ class User(ndb.Model):
             return True
 
         return False
-		
-		
-		
-		
--- MyLittleBuisness/web/pages/__init__.py
+++ MyLittleBuisness/web/pages/__init__.py
@@ -1,1 +1,5 @@
-__author__ = 'Yuri'
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+
