57c6cbef1fc9cefee578b582f48b1dff609d0a54
racheliel <racheli7722@gmail.com>
2015-05-26 20:31:31 +0300

    error

--- MyLittleBuisness/web/pages/home.py
+++ MyLittleBuisness/web/pages/home.py
@@ -6,25 +6,25 @@ import json
 class DefaultHandler(webapp2.RequestHandler):
 	def get(self):
 		template_params = {}
-		if self.request.cookies.get('session'):    
-			user = User.checkToken(self.request.cookies.get('session'))
-			self.redirect('/homeUserIn')
-			return
-		email = self.request.get('email')
-		user = None
-		if email:
-			self.response.write(json.dumps({'status':'OK'}))
-			user = User.checkIfUserExists(email)
-			if user:
-				self.response.set_cookie('session', str(user.key.id()))
-				self.redirect('/homeUserIn')
-				return
-			else:
-				user = User.addUser(email)
-				self.redirect('/homeUserIn')
-				self.response.set_cookie('session', str(user.key.id()))
-				return
-		else:
+	#	if self.request.cookies.get('session'):    
+	#		user = User.checkToken(self.request.cookies.get('session'))
+	#		self.redirect('/homeUserIn')
+	#		return
+	#	email = self.request.get('email')
+	#	user = None
+	#	if email:
+	#		self.response.write(json.dumps({'status':'OK'}))
+	#		user = User.checkIfUserExists(email)
+	#		if user:
+	#			self.response.set_cookie('session', str(user.key.id()))
+	#			self.redirect('/homeUserIn')
+	#			return
+	#		else:
+	#			user = User.addUser(email)
+	#			self.redirect('/homeUserIn')
+	#			self.response.set_cookie('session', str(user.key.id()))
+	#			return
+	#	else:
 			html = template.render("web/templates/home.html", template_params)
 			self.response.write(html)
 
