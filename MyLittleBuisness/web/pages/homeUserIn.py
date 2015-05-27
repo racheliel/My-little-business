5e981b8a3d30e2ea10ecb6a72b125dfce359220c
racheliel <racheli7722@gmail.com>
2015-05-27 10:13:48 +0300

    login

--- MyLittleBuisness/web/pages/homeUserIn.py
+++ MyLittleBuisness/web/pages/homeUserIn.py
@@ -1,7 +1,7 @@
 from google.appengine.ext.webapp import template
 from models.user import User
 import webapp2
-import HTML
+
 
 class IndexHandler(webapp2.RequestHandler):
 	def get(self):
@@ -9,12 +9,12 @@ class IndexHandler(webapp2.RequestHandler):
 		user = None
 		if self.request.cookies.get('session'):
 			user = User.checkToken(self.request.cookies.get('session'))
-#		if not user:
-#			self.redirect('/')
+		if not user:
+			self.redirect('/')
 		
-#		myEmail = user.email
+		myEmail = user.email
 
-#		template_params['emailUser'] = myEmail
+		template_params['emailUser'] = myEmail
 						
 		html = template.render("web/templates/homeUserIn.html", template_params)
 		self.response.write(html)
