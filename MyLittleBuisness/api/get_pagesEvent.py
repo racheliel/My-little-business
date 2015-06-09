c6094fa3c90ad4c8eac79f0f423028b104b8071b
racheliel <racheli7722@gmail.com>
2015-06-09 15:31:31 +0300

    uptate

--- MyLittleBuisness/api/get_pagesEvent.py
+++ MyLittleBuisness/api/get_pagesEvent.py
@@ -5,7 +5,7 @@ import json
 from models.user import User
 from models.event import Event
 
-class Getpages(webapp2.RequestHandler):
+class Getevents(webapp2.RequestHandler):
     def get(self):
         user = User.checkToken(self.request.cookies.get('session'))
         if self.request.cookies.get('our_token'):    #the cookie that should contain the access token!
@@ -16,10 +16,10 @@ class Getpages(webapp2.RequestHandler):
             self.response.write('No user - access denied')
             return
 
-        events = Event.etAllEvent(user)
+        events = Event.getAllEvent(user)
         self.response.write(json.dumps({"status": "OK", "events": events}))
 
 
 app = webapp2.WSGIApplication([
-    ('/api/get_pagesEvent', Getpages)
+    ('/api/get_pagesEvent', Getevents)
 ], debug=True)
